3aae8b4a3f1331bec0bdd27bd4b844705d47ef7f
Mohammednur <mamebb2023@gmail.com>
2023-04-29 09:56:09 +0300

    Selection Algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "srot.h"
+
+/**
+ * selection_sort - Selection sorting algroithm
+ * @array: the array
+ * @size: the size fo the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, index;
+	int tmp, swap, flag = 0;
+
+	if (array == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		tmp = i;
+		flag = 0;
+		for (index = i + 1; index < size; index++)
+		{
+			if (array[tmp] > array[index])
+			{
+				tmp = index;
+				flag += 1;
+			}
+		}
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
+		if (flag != 0)
+			print_array(array, size);
+	}
+}
